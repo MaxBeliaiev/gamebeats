8d6d29b5802d93e9527ffec3fd77a6632274b637
Maksym Beliaiev <maksym.beliaiev@idean.com>
2023-07-21 01:10:00 +0300

    Random

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,9 +1,7 @@
 import { NextResponse } from 'next/server'
 import { prisma } from '@/db'
-import { getAuthSession } from '@/lib/auth'
 
 export async function GET(req: Request) {
-  const session = getAuthSession()
   const matches = await prisma.match.findMany()
 
   return NextResponse.json({ hello: matches })
