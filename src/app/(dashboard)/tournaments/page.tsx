6f28258a089e65d2b3cc32bbe167ff9bad05631a
Max Beliaiev <maxbeliaiev@MacBook-Pro-Max.local>
2023-08-21 01:37:16 +0300

    Frontend tournaments sorting

--- src/app/(dashboard)/tournaments/page.tsx
+++ src/app/(dashboard)/tournaments/page.tsx
@@ -1,20 +1,12 @@
 import { prisma } from '@/db'
 import TournamentsClient from '@/app/client'
+import {Tournament, TournamentStatus} from "@prisma/client";
 
 const Dashboard = async () => {
   const tournaments = await prisma.tournament.findMany({
     orderBy: [
       {
-        endedAt: 'asc',
-      },
-      {
-        status: 'desc',
-      },
-      {
         startedAt: 'asc',
-      },
-      {
-        id: 'desc'
       },
     ],
     take: 10,
@@ -23,6 +15,23 @@ const Dashboard = async () => {
     },
   })
 
-  return <TournamentsClient data={tournaments} />
+  const sortedTournaments = tournaments.sort((a: Tournament, b: Tournament) => {
+    if (a.status === TournamentStatus.ONGOING) {
+      return -1
+    }
+    if (b.status === TournamentStatus.ONGOING) {
+      return 1
+    }
+    if (a.status === TournamentStatus.FINISHED) {
+      return b.status !== TournamentStatus.FINISHED ? 1 : b.id - a.id
+    }
+    if (b.status === TournamentStatus.FINISHED) {
+      return -1
+    }
+
+    return 0
+  })
+
+  return <TournamentsClient data={sortedTournaments} />
 }
 export default Dashboard
