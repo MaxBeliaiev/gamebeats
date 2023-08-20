464d17c0fa4f7755ee7e1eceef24007a8497f61b
Max Beliaiev <maxbeliaiev@MacBook-Pro-Max.local>
2023-08-21 02:46:36 +0300

    Adding CRON jobs

---
+++ src/app/api/weekly-ufc-rating/route.ts
@@ -0,0 +1,30 @@
+import { NextResponse } from 'next/server'
+import { prisma } from '@/db'
+
+export async function GET() {
+
+  try {
+    const rating = {
+      data: 'some UFC rating',
+      updatedAt: new Date()
+    }
+
+    await prisma.data.upsert({
+      where: {
+        uniqueId: 'ufc_weekly_rating'
+      },
+      update: {
+        json: rating,
+      },
+      create: {
+        uniqueId: 'ufc_weekly_rating',
+        json: rating,
+      },
+    },)
+
+    return NextResponse.json(rating)
+  } catch (error) {
+    console.log('[UFC_WEEKLY_RATING_ERROR]', error)
+    return new NextResponse('Internal error', { status: 500 })
+  }
+}
