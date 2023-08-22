488f986d98f37fbd2aa9b564db4ae4b6879d2890
Max Beliaiev <maxbeliaiev@MacBook-Pro-Max.local>
2023-08-23 00:38:34 +0300

    Get UFC ratings endpoint

---
+++ src/app/api/weekly-ufc-rating/route.ts
@@ -0,0 +1,19 @@
+import { NextResponse } from 'next/server'
+import { prisma } from '@/db'
+
+export async function GET() {
+  try {
+    const ufcRating = await prisma.data.findUnique({
+      where: {
+        uniqueId: 'ufc_weekly_rating'
+      }
+    })
+
+    return NextResponse.json({ data: ufcRating, cached: new Date() })
+  } catch (error) {
+    console.log('[UFC_WEEKLY_RATING_ERROR]', error)
+    return new NextResponse('Internal error', { status: 500 })
+  }
+}
+
+export const revalidate = 3600;
