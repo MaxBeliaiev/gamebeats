13df3190ac686962eb4e54e4b48254d51c73e4d9
Maksym Beliaiev <maksym.beliaiev@idean.com>
2023-09-06 00:15:25 +0300

    Revalidate test many

--- src/app/api/revalidate-test/route.ts
+++ src/app/api/revalidate-test/route.ts
@@ -4,13 +4,14 @@ import { prisma } from '@/db'
 export async function GET(req: Request) {
   try {
     // let fullFields = Boolean(getAuthSession())
-    // const { searchParams } = new URL(req.url)
+    const { searchParams } = new URL(req.url)
 
-    // const take = Number(searchParams.get('size')) || 10
-    // const page = Number(searchParams.get('page')) || 0
+    const take = Number(searchParams.get('size')) || 10
+    const page = Number(searchParams.get('page')) || 0
 
     const data = await prisma.match.findMany({
-      take: 10,
+      take,
+      // skip: page ? (page - 1) * take : 0,
       orderBy: {
         id: 'desc',
       },
