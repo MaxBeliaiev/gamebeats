2a85e304e647455c300a516465bde70a67054aa1
Maksym Beliaiev <maksym.beliaiev@idean.com>
2023-09-26 15:08:39 +0300

    Updated stream links

--- src/lib/constants/matches.ts
+++ src/lib/constants/matches.ts
@@ -1,6 +1,6 @@
 export const STREAM_CHANNELS = ['1', '2']
 
 export const streamLinks = {
-  [STREAM_CHANNELS[0]]: 'https://www.twitch.tv/gamebeats_ufc',
-  [STREAM_CHANNELS[1]]: 'https://www.twitch.tv/gamebeats_ufc_2'
+  [STREAM_CHANNELS[0]]: 'https://twitch.tv/highkick_1',
+  [STREAM_CHANNELS[1]]: 'https://twitch.tv/highkick_2'
 }
