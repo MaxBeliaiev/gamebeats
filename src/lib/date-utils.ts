dbb57769be1dddfed5911bc75ecfc2535eb9064e
Maksym Beliaiev <lightod@gmail.com>
2023-11-16 19:07:37 +0300

    Tournament results for payment page

--- src/lib/date-utils.ts
+++ src/lib/date-utils.ts
@@ -4,3 +4,5 @@ const dateTimeFormat = 'dd/MM/yyyy H:mm'
 
 export const formatDateTime = (date: any, formatStr = dateTimeFormat) =>
   date instanceof Date ? format(date, formatStr) : format(parseISO(date), formatStr)
+
+
