9525bdaf160bc48e20b376a8e9cb68fffa553105
Maksym Beliaiev <lightod@gmail.com>
2024-01-22 00:44:58 +0300

    Added mask input for End time

--- src/lib/schemas/game.ts
+++ src/lib/schemas/game.ts
@@ -7,7 +7,8 @@ export const gameFinishFormSchema = z.object({
   }),
   endTime: z.string({
     required_error: 'Win time is required in format 0:00'
-  }).length(4, { message: "Wrong format" }),
+  }).regex(new RegExp('^([0-5]):[0-5][0-9]$'), { message: 'Wrong time format' }),
+    // .length(4, { message: "Wrong format" }),
   endMethod: z.string({
     required_error: 'Win method is required'
   })
