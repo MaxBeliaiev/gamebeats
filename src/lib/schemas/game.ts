2e96f23148a3fb8a0b223a3a9b3a22443a91240d
Maksym Beliaiev <lightod@gmail.com>
2024-02-12 23:51:12 +0300

    Fixed draw results

--- src/lib/schemas/game.ts
+++ src/lib/schemas/game.ts
@@ -8,17 +8,8 @@ export const gameFinishFormSchema = z.object({
   endTime: z.string({
     required_error: 'Win time is required in format 0:00'
   }).regex(new RegExp('^([0-5]):[0-5][0-9]$'), { message: 'Wrong time format' }),
-  endMethod: z.string({
-    required_error: 'Win method is required'
-  })
-    .transform((value, ctx): string | undefined => {
-      if (value == '') {
-        ctx.addIssue({
-          code: 'custom',
-          message: 'Win method is required',
-        })
-      } else {
-        return value
-      }
-    }),
+  endMethod: z.string()
+}).refine(({ endMethod, winnerId }) => !(winnerId && !endMethod), {
+  message: 'Win method is required.',
+  path: ['endMethod'],
 })
