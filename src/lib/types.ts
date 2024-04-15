5da1f15947c23dce166524eacf788da038757f2c
Maksym Beliaiev <lightod@gmail.com>
2024-04-15 03:54:04 +0300

    Fixed TS

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,7 +1,12 @@
 import { PrismaClient } from '@prisma/client'
-import { DefaultArgs, PrismaClientOptions } from 'prisma/prisma-client/runtime/library'
+import { DefaultArgs } from 'prisma/prisma-client/runtime/library'
+import { Prisma } from '.prisma/client'
 
 type TransactionPrisma =
-  Omit<PrismaClient<PrismaClientOptions, never, DefaultArgs>, "$connect" | "$disconnect" | "$on" | "$transaction" | "$use" | "$extends">
+  PrismaClient<Prisma.PrismaClientOptions, never, DefaultArgs>
 
-export type PrismaClientCommon = PrismaClient | TransactionPrisma
+export type PrismaTransactionalClient = Parameters<
+  Parameters<PrismaClient['$transaction']>[0]
+>[0];
+
+export type PrismaClientCommon = PrismaClient | PrismaTransactionalClient | TransactionPrisma
